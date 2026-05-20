956cefa7e687dd272285b71a35117b1484fcb1cc
leegyeongsik <leegs1012@naver.com>
2026-05-20 21:56:18 +0900

    init_registration_by_class_method

--- src/main/java/io/jacksoon/router/init/factory/InitFactory.java
+++ src/main/java/io/jacksoon/router/init/factory/InitFactory.java
@@ -1,63 +1,54 @@
 package io.jacksoon.router.init.factory;
 
 import io.jacksoon.router.init.annotation.Init;
+import io.jacksoon.router.init.registration.InitMapper;
 import org.reflections.Reflections;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import static io.jacksoon.router.init.registration.Registration.create;
+import static io.jacksoon.router.init.registration.Registration.scan;
 
 public class InitFactory {
-    static Map<Class<?>, Set<String>> typeMap = new HashMap<>();
-    static Map<String, Object> nameMap = new HashMap<>();
+    static Map<String, InitMapper> initMap = new HashMap<>();
     public static void initialize() {
-        if (!typeMap.isEmpty()) {
-            return;
-        }
         init();
     }
-
     private static void init() {
         Reflections reflections = new Reflections("io.jacksoon.router");
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(
                 io.jacksoon.router.init.annotation.Init.class
         );
+
+
         for (Class<?> clazz : classes) {
             Init init = clazz.getAnnotation(Init.class);
             String name = init.value();
-
             if (name.isEmpty()) {
                 name = clazz.getSimpleName();
             }
-            try {
-                create(clazz, name);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            scan(clazz,name);
+            for (String identifier : initMap.keySet()) {
+                try {
+                    create(initMap.get(identifier));
+                } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
 
-    public static Object get(Class<?> clazz,String name) {
-        if(!typeMap.containsKey(clazz)){
-            return null;
-        }
-        if(!typeMap.get(clazz).contains(name)){
-            return null;
-        }
-        return nameMap.get(name);
+    public static InitMapper get(String name) {
+        return initMap.get(name);
     }
-    public static void put(Object object, String name,Class<?>clazz) {
-        Set<String> set;
-        if (!typeMap.containsKey(clazz)) {
-            set = new HashSet<>();
-            typeMap.put(clazz, set);
-        } else {
-            set = typeMap.get(clazz);
-        }
-        set.add(name);
-        nameMap.put(name, object);
+    public static void scanPutClass(Class<?> clazz, String name) {
+        initMap.put(name,new InitMapper(clazz,"CLASS"));
+    }
+    public static void scanPutMethod(Class<?> clazz, Method method, String name,String parent) {
+        initMap.put(name,new InitMapper(clazz,"METHOD",method,parent));
     }
 }
--- src/main/java/io/jacksoon/router/init/registration/Registration.java
+++ src/main/java/io/jacksoon/router/init/registration/Registration.java
@@ -4,38 +4,67 @@ import io.jacksoon.router.init.annotation.Init;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Objects;
 
-import static io.jacksoon.router.init.factory.InitFactory.get;
-import static io.jacksoon.router.init.factory.InitFactory.put;
+import static io.jacksoon.router.init.factory.InitFactory.*;
 
 public class Registration {
-    static public Object create(Class clazz, String name) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        if (name.isEmpty()) {
-            name = clazz.getSimpleName();
+    static public InitMapper create(InitMapper initMapper) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        if (initMapper.getObject()!=null) {
+            return initMapper;
         }
-        if (get(clazz, name) != null) {
-            return get(clazz, name);
-        }
-        Constructor<?> constructor = clazz.getConstructors()[0];
-        Class<?>[] ca = constructor.getParameterTypes();
-        Object[] objects = new Object[ca.length]; // 이렇게했을때 만약에 생성을 메서드로 하면 걔의 생성자가 있겠지? 근데 그걸로 생성하지않고 메서드로 생성하는게 맞는거일수있는데
-        for (int i = 0; i < objects.length; i++) { //이렇게하면 걔의 생성자로 생성해서 찐빠가 날수있다
-                                                    // 그래서 스캔을 하면 걔 파라미터는 어디에있고 걔는 어떻게 만들어야한다 라는게 정의가 되어있기때문에 잘 만들듯?
-            Init init = constructor.getParameters()[i].getAnnotation(Init.class);
-            String paraName;
-            paraName = (Objects.requireNonNull(init).value());
-            if (paraName.isEmpty()) {
-                paraName = ca[i].getSimpleName();
+        if(Objects.equals(initMapper.getType(), "CLASS")){
+            Constructor<?> constructor = initMapper.getClazz().getConstructors()[0];
+            Class<?>[] ca = constructor.getParameterTypes();
+            Object[] objects = new Object[ca.length];
+            for (int i = 0; i < objects.length; i++) {
+                Init init = constructor.getParameters()[i].getAnnotation(Init.class);  // 생성자 파라미터 만들어오셈
+                String parameterName;
+                if (init != null && !init.value().isEmpty()) {
+                    parameterName = init.value();
+                } else {
+                    parameterName = ca[i].getSimpleName();
+                }
+                InitMapper classInitMapper = get(parameterName);
+                objects[i] = create(classInitMapper).getObject();
             }
-            if (get(ca[i], paraName) == null) {
-                objects[i] = create(ca[i], paraName);
-            } else {
-                objects[i] = get(ca[i], paraName);
+            Object instance = constructor.newInstance(objects);
+            initMapper.setObject(instance);
+        }else {
+            Object parent = create(get(initMapper.getParent())).getObject(); // 부모부터 만들고 파라미터 만드셈
+            Method method =  initMapper.getMethod();
+
+            Class<?>[] para = method.getParameterTypes();
+            Object[] objects = new Object[para.length];
+            for (int i = 0; i < objects.length; i++) { // 부모만들었으면 메서드 파라미터 만들어오셈
+                Init init = method.getParameters()[i].getAnnotation(Init.class);
+                String parameterName;
+                if (init != null && !init.value().isEmpty()) {
+                    parameterName = init.value();
+                } else {
+                    parameterName = para[i].getSimpleName();
+                }
+                objects[i] = create(get(parameterName)).getObject();
             }
+            initMapper.setObject(method.invoke(parent,objects));
         }
-        Object instance = constructor.newInstance(objects);
-        put(instance, name, clazz);
-        return get(clazz, name);
+        return initMapper;
     }
+    static public void scan(Class<?> clazz,String name){
+
+        scanPutClass(clazz,name);
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            Init init = method.getAnnotation(Init.class);
+            if(init!=null){
+               String methodName = init.value();
+               if(methodName.isEmpty()){
+                   methodName = method.getName();
+               }
+               scanPutMethod(clazz,method,methodName,name);
+            }
+        }
+    }
+
 }
