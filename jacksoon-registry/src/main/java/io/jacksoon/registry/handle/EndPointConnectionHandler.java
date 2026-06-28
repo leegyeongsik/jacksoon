69b8eeb22d3aeb67dec202d0d0a70878f13f85d2
leegyeongsik <leegs1012@naver.com>
2026-06-29 05:12:22 +0900

    registry 뼈대 추가

--- jacksoon-registry/src/main/java/io/jacksoon/registry/handle/EndPointConnectionHandler.java
+++ jacksoon-registry/src/main/java/io/jacksoon/registry/handle/EndPointConnectionHandler.java
@@ -3,4 +3,6 @@ package io.jacksoon.registry.handle;
 public class EndPointConnectionHandler {
 
     //얘는 일단 커넥션에서 엔드포인트맺고 엔드포인트에 주기적으로 호출할거임
+    // 여기도 식별할수있는거 찾고
+    // 만약에 실패났으면 queue에 식별정보 보내셈 삭제해라고 그랬을떄 regi , store 에 가서 삭제해야댐
 }
--- jacksoon-registry/src/main/java/io/jacksoon/registry/pipeline/dispatcher/RegistryDispatcher.java
+++ jacksoon-registry/src/main/java/io/jacksoon/registry/pipeline/dispatcher/RegistryDispatcher.java
@@ -5,4 +5,8 @@ public class RegistryDispatcher {
     // 여기서 request정보로 찾아줄거임 다음뎁스 얘는 그럼 map으로 들고있어야할듯
     // map으로 가령 request/ 이거면 snapshot주고 이런식으로 다 등록해놓고 config에서
 
+    //컨피그에서 /a -> requestRegister , /b -> registrySnapshotQuery 이런식으로 등록
+    // pipeline 인터페이스화해서 각 디렉토리에서 맞는거 구현하게끔 하자 즉 execute부분 구현
+    // 레지스트리는 PipelineExecutor 객체 찾아서 context에 넣어주고 execute실행하는 식으로
+
 }
